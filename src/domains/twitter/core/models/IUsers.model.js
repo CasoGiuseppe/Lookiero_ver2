ca0481861b9b464e12fa47b65f14cedcb9416d13
gcaso <76.giuseppe.caso@gmail.com>
2022-09-29 16:04:02 +0200

    fix user interface

--- src/domains/twitter/core/models/IUsers.model.js
+++ src/domains/twitter/core/models/IUsers.model.js
@@ -2,6 +2,6 @@ export default class IUsers {
   constructor(payload = {}) {
     this.id = payload.id || null;
     this.author = payload.data.name || null;
-    this.following = payload.following || null;
+    this.following = payload.following;
   }
 }
